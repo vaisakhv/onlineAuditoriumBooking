0765440253a2f3cdbd95c8931a7e4ea5f93abcf2
HarikrishnanC903 <Hari.Chandranpillai@ust-global.com>
2016-08-18 18:09:30 +0530

    Entire jsf by hari

---
+++ LoginForAuditorium/WebContent/ForgetPasswordFailed.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title></title>
+<style>
+body {
+	color: white;
+	background-image: url("Auditor.jpg");
+	background-position: cover;
+	text-align: center;
+}
+</style>
+</head>
+<body>
+<p>##ERROR##</p>
+<p>The password you enter is not matching the above password please try again later</p>
+<a href="http://localhost:8082/LoginForAuditorium/ForgetPassword.ust">GOBACK</a>
+</body>
+</html>
---
+++ LoginForAuditorium/src/com/ust_global/Login.java
@@ -0,0 +1,103 @@
+package com.ust_global;
+
+import java.util.Properties;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpSession;
+import com.ust_global.user.LoginCredentialsBeanRemote;
+import com.ust_global.user.User;
+
+public class Login {
+
+	String userName;
+	String passWord;
+	String confirmPassword;
+
+	public Login() {
+
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWord() {
+		return passWord;
+	}
+
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String login() throws NamingException {
+		String msg = " ";
+		Properties p = new Properties();
+		p.put(Context.PROVIDER_URL, "localhost:1099");
+		p.put(Context.INITIAL_CONTEXT_FACTORY,
+				"org.jnp.interfaces.NamingContextFactory");
+		p.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
+		Context ctx;
+		ctx = new InitialContext(p);
+		LoginCredentialsBeanRemote log = (LoginCredentialsBeanRemote) ctx.lookup("LoginCredentialsBean/remote");
+		User user=log.Login(this.userName, this.passWord);
+		if ( user!= null)// will return a User object
+		{
+			if (this.passWord.equals(user.getPassWord())
+					&& this.userName.equals(user.getUserName())) {
+				FacesContext fc = FacesContext.getCurrentInstance();
+				ExternalContext ec = fc.getExternalContext();
+				HttpSession session = (HttpSession) ec.getSession(false);
+				session.setAttribute("userName", this.userName);
+				msg = "SuccessLogin";
+			} else {
+				msg = "failurePassword";
+			}
+			// invalid password
+		} else {
+			msg = "failureUsername";// invalid username
+		}
+		return msg;
+	}
+
+	public String forgotPassword() throws NamingException {
+		String msg = " ";
+		Properties p = new Properties();
+		p.put(Context.PROVIDER_URL, "localhost:1099");
+		p.put(Context.INITIAL_CONTEXT_FACTORY,
+				"org.jnp.interfaces.NamingContextFactory");
+		p.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");
+		Context ctx;
+		ctx = new InitialContext(p);
+		LoginCredentialsBeanRemote log = (LoginCredentialsBeanRemote) ctx
+				.lookup("LoginCredentialsBean/remote");
+		if (this.passWord.equals(this.confirmPassword)) {
+			User forgetPass = log.forgotPassword(this.userName, this.passWord);
+			if (forgetPass != null) {
+				forgetPass.setPassWord(passWord);
+				msg = "passUpdated";
+			} else {
+				msg = "forgetusernamefailed";// username invalid
+			}
+		} else {
+			msg = "forgetpasswordfailed";// passwords dont match
+		}
+		return msg;
+
+	}
+
+}
